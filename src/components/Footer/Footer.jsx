51aed667dc5878a66afa34170013c0dddef62844
Aline Zsigmond <alinezsigmond@hotmail.com>
2024-02-06 18:40:59 -0300

    bug corrections

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,7 @@ import Logo from '../../assets/img/logo/logoX.svg'
 
 export default function Footer() {
     return(
-        <footer>
+        <footer className='footer'>
             <p className='footer-text'>‘Buckets by Alan’ is a joke and should not be taken seriously (but if you really want a custom bucket maybe try contacting Alan).</p>
             <img src={Logo} alt="Logo Buckets by Alan" />
             <p className="copyright">© 2024 Aline Zsigmond (aka Pokemond) - All rights reserved</p>
