78abce6d7b5dfc0bed62f1daf5dfab1776bf5ff5
Aline Zsigmond <alinezsigmond@hotmail.com>
2024-02-06 18:23:22 -0300

    added shop page

---
+++ src/pages/shop/Shop.jsx
@@ -0,0 +1,25 @@
+import Footer from "../../components/Footer/Footer";
+import Header from "../../components/Header/Header";
+import Title from "../../components/Title/Title";
+import Item from "../../components/item/Item";
+
+import products from '../../../api/buckets-api.json'
+
+import './shop.css'
+
+export default function Shop() {
+    return (
+        <>
+            <Header />
+            <Title text='Shop' />
+            <div className="items-wrapper">
+                <div className="items-grid">
+                {products.map((item, index) => (
+                    <Item key={index} title={item.title} price={item.price} img={item.img} alt={item.alt} />
+                ))}
+                </div>
+            </div>
+            <Footer />
+        </>
+    )
+}
