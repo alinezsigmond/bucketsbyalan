fe44383ab1b2f904021084ed4e44f43b437235ec
Aline Zsigmond <alinezsigmond@hotmail.com>
2024-02-07 14:36:03 -0300

    updated links on header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,19 @@
 import './header.css'
 import logoH from '../../assets/img/logo/logoX.svg'
 import shopIcon from '../../assets/img/shop.svg'
+import { Link } from 'react-router-dom'
+import Shop from '../../pages/shop/Shop'
 
 export default function Header() {
     return(
         <header>
             <div className="logo">
-                <img src={logoH} alt="Logo Buckets by Alan" />
+                <Link to='/'><img src={logoH} alt="Logo Buckets by Alan" /></Link>
             </div>
             <div className="section2">
                 <nav className="links">
                     <a href="#">About us</a>
-                    <a href="#">Shop</a>
+                    <Link  to='/shop'>Shop</Link>
                     <a href="#">Merch</a>
                     <a href="#">Contact</a>
                 </nav>
