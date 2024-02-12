86d95bb73eaa794d50adcef3356a6c1c191afbf8
Aline Zsigmond <alinezsigmond@hotmail.com>
2024-02-12 18:31:02 -0300

    added a proper form to contact us page

--- src/pages/contact-us/ContactUs.jsx
+++ src/pages/contact-us/ContactUs.jsx
@@ -1,14 +1,22 @@
+import { useState } from "react";
 import Footer from "../../components/Footer/Footer";
 import Header from "../../components/Header/Header";
+import Modal from "../../components/Modal/Modal";
 import Title from "../../components/Title/Title";
 
 import './contact-us.css'
 
 export default function ContactUs() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [subject, setSubject] = useState('');
+    const [message, setMessage] = useState('');
+    const fullMessage = {name,email,subject,message}
     return(
         <>
         <Header />
         <Title text='Contact us' />
+        <Modal isOpen={true} />
         <div className="contact_wrapper">
             <p className="contact_text">
                 Do you have any suggestions, compliments, or complaints? 
@@ -16,16 +24,37 @@ export default function ContactUs() {
                 feeling lonely and in need of a friend? Don’t hesitate 
                 to reach out!
             </p>
-            <form action="" className="contact_form">
+            <form onSubmit={(event) => event.preventDefault()} className="contact_form">
                 <label>Name</label>
-                <input type="text" className="contact_input" id="" />
+                <input 
+                    value={name} 
+                    onChange={(e) => setName(e.target.value)} 
+                    type="text" 
+                    className="contact_input"
+                />
                 <label>E-mail</label>
-                <input type="email" className="contact_input" id="" />
+                <input 
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)} 
+                    type="email" 
+                    className="contact_input" 
+                />
                 <label>Subject</label>
-                <input type="text" className="contact_input" id="" />
+                <input 
+                    value={subject} 
+                    onChange={(e) => setSubject(e.target.value)} 
+                    type="text" 
+                    className="contact_input"
+                />
                 <label>Message</label>
-                <textarea type="text" className="contact_input" id="contact_message" />
-                <button className="contact_button">Send</button>
+                <textarea
+                    value={message} 
+                    onChange={(e) => setMessage(e.target.value)} 
+                    type="text" 
+                    className="contact_input" 
+                    id="form_message" 
+                />
+                <button onClick={() => console.log(fullMessage)} className="contact_button">Send</button>
             </form>
         </div>
         <Footer />
