64c7b38aaab47ecb5446129b3ca218330a7f7e19
Aline Zsigmond <alinezsigmond@hotmail.com>
2024-04-26 16:09:50 -0300

    created mobile menu component

---
+++ src/components/Menu.jsx
@@ -0,0 +1,16 @@
+import { Link } from 'react-router-dom'
+
+export default function Menu() {
+    const menu = document.getElementById('menu-mobile');
+    console.log(menu);
+    return (
+        <div
+            id='menu-mobile'
+            className='bg-[#D9D9D9] absolute w-7/12 left-0 top-52 divide-y divide-solid captalize transform'>
+            <div className='py-4 px-9'><Link to='/about-us'>About us</Link></div>
+            <div className='py-4 px-9'><Link to='/shop'>Shop</Link></div>
+            <div className='py-4 px-9'><Link to='/merch'>Merch</Link></div>
+            <div className='py-4 px-9'><Link to='/contact-us'>Contact us</Link></div>
+        </div>
+    )
+}
