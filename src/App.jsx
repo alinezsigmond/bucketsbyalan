a0ff173f855fffc56e15012ca3f5679d8105bf11
Aline Zsigmond <alinezsigmond@hotmail.com>
2024-04-26 22:42:13 -0300

    added all pages to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import Error404 from './pages/404/404';
+import About from './pages/about/About';
+import ContactUs from './pages/contact-us/ContactUs';
 import Home from './pages/home/Home'
+import Merch from './pages/merch/Merch';
 import Shop from './pages/shop/Shop'
-import './reset.css'
-import './vars.css'
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -12,7 +14,10 @@ function App() {
       <Routes>
         <Route path="/" exact element={<Home />} />
         <Route path="/shop" element={<Shop />} />
-        {/* <Route path="*" element={<Erro404 />} /> */}
+        <Route path="/merch" element={<Merch />} />
+        <Route path="/about-us" element={<About />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="*" element={<Error404 />} />
       </Routes>
     </BrowserRouter>
   )
