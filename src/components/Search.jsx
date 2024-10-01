f4cb311b10ebbc19bcb93b22cfaae9cf68accae2
Aline Zsigmond <alinezsigmond@hotmail.com>
2024-09-30 21:36:49 -0300

    slightly improved search bar

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,22 +1,16 @@
 import data from '../../api/products.json'
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function SearchBar() {
     const [searchTerm, setSearchTerm] = useState('');
-    const [filteredData, setFilteredData] = useState(data);
+    const [filteredResults, setFilteredResults] = useState([]);
 
-    const handleInputChange = (event) => {
-        const { value } = event.target;
-        setSearchTerm(value);
-        filterData(value);
-    };
-
-    const filterData = (searchTerm) => {
-        const filteredData = data.filter((item) =>
+    useEffect(() => {
+        const results = data.filter(item =>
             item.title.toLowerCase().includes(searchTerm.toLowerCase())
         );
-        setFilteredData(filteredData);
-    };
+        setFilteredResults(results);
+    }, [searchTerm, data]);
 
     return (
         <div className='flex flex-col'>
@@ -26,15 +20,15 @@ export default function SearchBar() {
                 id="searchInput"
                 className='bg-white rounded-lg w-56 h-9 px-3 text-lg focus:outline-none'
                 value={searchTerm}
-                onChange={handleInputChange}
+                onChange={(e) => setSearchTerm(e.target.value)}
             ></input>
-            {searchTerm !== '' ?
+            {searchTerm !== '' && filteredResults.length > 0 ?
                 <div
                     id='results'
                     className='bg-white w-56 divide-y divide-solid absolute mt-9 border border-b-[#D9D9D9] border-x-[#D9D9D9]'
                 >
-                    {filteredData.map((item) => (
-                        <div className='flex items-center justify-around hover:bg-black/5'>
+                    {filteredResults.map((item) => (
+                        <div className='flex items-center justify-around hover:bg-black/5' key={item.id}>
                             <img className='size-14 object-cover' src={item.img} alt={item.alt} />
                             <div className='w-32 text-left py-4'>
                                 <p className='font-text text-sm capitalize' key={item.id}>{item.title}</p>
