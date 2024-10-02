50b8716a94ccbaef21a7599c633579e35fd99bda
Aline Zsigmond <alinezsigmond@hotmail.com>
2024-10-02 15:02:20 -0300

    updated menu's styling

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom'
 export default function Menu() {
     return (
         <div
-            id='menu-mobile'
-            className='bg-[#D9D9D9] absolute w-7/12 left-0 top-52 divide-y divide-solid captalize transform'>
+            id='menu'
+            className='bg-[#D9D9D9] absolute w-56 left-0 top-16 divide-y divide-solid captalize transform'>
             <div className='py-4 px-9'><Link to='/about-us'>About us</Link></div>
             <div className='py-4 px-9'><Link to='/shop'>Shop</Link></div>
             <div className='py-4 px-9'><Link to='/merch'>Merch</Link></div>
