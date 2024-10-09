3a83e766cc8cd066b72acf2a41587976ad66764c
Aline Zsigmond <alinezsigmond@hotmail.com>
2024-10-09 14:51:26 -0300

    added toast for every item added to cart

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,6 +1,16 @@
 import { addToCart } from "./Header"
+import { Slide, toast } from 'react-toastify';
+
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function Item(props) {
+    const notify = () => toast.success("Item added to cart", {
+        autoClose: 3000,
+        position: 'bottom-right',
+        pauseOnHover: false,
+        transition: Slide
+    });
+
     return (
         <div className="border border-black border-solid p-4 m-3">
             <img src={props.img} alt={props.alt} className="self-center h-[150px] w-[150px] object-cover" />
@@ -13,6 +23,7 @@ export default function Item(props) {
                     className="bg-black text-[#D9D9D9] font-text w-32 h-8 self-center hover:bg-teal-950"
                     data-name={props.title}
                     data-price={props.price}
+                    onMouseUp={notify}
                     onClick={() => {
                         addToCart(props.title, props.price, props.img, props.alt);
                     }}
